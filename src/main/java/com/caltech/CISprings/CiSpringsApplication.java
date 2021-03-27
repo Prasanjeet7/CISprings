568ba802d4995a573e6c8ceb7435fdacf068034c
student <prasanjeet.sahoo@gmail.com>
2021-03-27 17:12:36 +0000

    ading a jenkins file

---
+++ src/main/java/com/caltech/CISprings/CiSpringsApplication.java
@@ -0,0 +1,13 @@
+package com.caltech.CISprings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiSpringsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiSpringsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caltech/CISprings/CiSpringsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caltech.CISprings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiSpringsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
